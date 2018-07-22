d5e5aba1c540b770606585562a4cd735c57036c7
triinoxys <contact@triinoxys.fr>
2018-07-23 01:41:16 +0200

    Add Looper module
    A simple module which runs a loop `n` times

--- src/crystal_test_app.cr
+++ src/crystal_test_app.cr
@@ -3,7 +3,8 @@ require "benchmark"
 
 # Base module for `CrystalTestApp`
 module CrystalTestApp
-  CHOICES = ["Hello World"] of String
+  CHOICES = ["Hello World",
+             "Looper"] of String
 
   puts "Welcome to this Crystal test app!"
   puts "If you expect to see something useful here, you are not at the good place :-)\n\n"
---
+++ src/crystal_test_app/looper.cr
@@ -0,0 +1,16 @@
+module CrystalTestApp
+  module Modules
+    class Looper
+      def initialize
+        puts "How many times do you want to loop ?"
+        n = gets
+        if n.as(String).to_i?
+          n.as(String).to_i.times { |i| puts "Iteration ##{i + 1}" }
+        else
+          puts "Please provide a valid number."
+          self.initialize
+        end
+      end
+    end
+  end
+end
