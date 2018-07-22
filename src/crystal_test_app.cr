49f975d33e3b0195616f36ce3eeab02806ace596
triinoxys <contact@triinoxys.fr>
2018-07-23 01:35:47 +0200

    Add program base
    Futures modules will be added easily thanks to Crystal's
    meta-programming capabilites.
    A `case` is auto-generated with all modules, all of this
    at compile time (not run-time!)

--- src/crystal_test_app.cr
+++ src/crystal_test_app.cr
@@ -1,6 +1,36 @@
 require "./crystal_test_app/*"
+require "benchmark"
 
-# TODO: Write documentation for `CrystalTestApp`
+# Base module for `CrystalTestApp`
 module CrystalTestApp
-  # TODO: Put your code here
+  CHOICES = [] of String
+
+  puts "Welcome to this Crystal test app!"
+  puts "If you expect to see something useful here, you are not at the good place :-)\n\n"
+
+  menu(true)
+
+  def self.menu(first_run = false)
+    puts "\n--------------------\n" unless first_run
+    puts "Please, choose the module you want to launch:"
+
+    CHOICES.each_with_index do |choice, index|
+      puts "#{index + 1}. #{choice}"
+    end
+    puts "0. Quit\n"
+
+    {% begin %}
+    case gets
+      {% for choice, index in CHOICES %}
+      when "{{index + 1}}"
+        CrystalTestApp::Modules::{{choice.strip.tr(" ", "").id}}.new
+      {% end %}
+      when "0"
+        puts "Goodbye!"
+        exit 0
+      else
+        menu
+    end
+    {% end %}
+  end
 end
